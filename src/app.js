963f76bfef2b0aa3ac3198d839ef529497c2a7b9
DjangoCzech <janpanus@gmail.com>
2023-04-19 23:11:45 +0200

    change float value of temp

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ fetch(urlLabe)
     .then(response => response.json())
     .then(data => {
         var t = document.getElementById("teplota-labe");
+        var tchange = data[0]["teplota"]
+        var tfloat = parseFloat(tchange.replace(",", ".").replace(' ',''))
         var d = document.getElementById("actual-date");
         var s = document.getElementById("stav-labe");
         var p = document.getElementById("prutok-labe");
-        t.innerHTML = `Teplota ${data[0]["teplota"]} °C`;
+        t.innerHTML = `Teplota ${tfloat} °C`;
         d.innerHTML = `Měřeno: ${data[0]["datum"]}`;
         s.innerHTML = `Stav: ${data[0]["stav"]} cm`;
         p.innerHTML = `Průtok: ${data[0]["prutok"]} m<sup>3</sup>s<sup>-1</sup>`;
