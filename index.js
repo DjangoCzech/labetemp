8786e226ccea52ce79907f240cf8b0d4730798ab
DjangoCzech <janpanus@gmail.com>
2023-04-20 10:52:12 +0200

    add Loucna results

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const axios = require("axios");
 const fs = require("fs");
 const cheerio = require("cheerio");
@@ -7,19 +7,18 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-const url = "https://www.theguardian.com/uk-news";
 const urlLabe =
   "https://hydro.chmi.cz/hppsoldv/popup_hpps_prfdyn.php?seq=307338";
-
-
+const urlLoucna =
+  "https://hydro.chmi.cz/hppsoldv/popup_hpps_prfdyn.php?seq=307363";
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
-app.get('/', function(req, res)  {
-  res.send('Hello World');
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
 
-app.get('/results', function(req, res)  {
+app.get("/results", function (req, res) {
   axios(urlLabe).then((response) => {
     const htmlLabe = response.data;
     const $ = cheerio.load(htmlLabe);
@@ -38,10 +37,37 @@ app.get('/results', function(req, res)  {
         });
       }
     });
-    res.json(teplotyLabe);    
+    res.json(teplotyLabe);
     const datumLabe = teplotyLabe[0]["datum"];
     const stavLabe = teplotyLabe[0]["stav"];
     const prutokLabe = teplotyLabe[0]["prutok"];
-    const teplotaLabe = teplotyLabe[0]["teplota"];  
+    const teplotaLabe = teplotyLabe[0]["teplota"];
+  });
+});
+
+app.get("/resultsloucna", function (req, res) {
+  axios(urlLoucna).then((response) => {
+    const htmlGet = response.data;
+    const $ = cheerio.load(htmlGet);
+    const tempsGet = [];
+    $(".center_text tr", htmlGet).each(function () {
+      const datum = $(this).find("td").eq(0).text();
+      const stav = $(this).find("td").eq(1).text();
+      const prutok = $(this).find("td").eq(2).text();
+      var teplota = $(this).find("td").eq(3).text();
+      if (teplota) {
+        tempsGet.push({
+          datum,
+          stav,
+          prutok,
+          teplota,
+        });
+      }
+    });
+    res.json(tempsGet);
+    const datumLabe = tempsGet[0]["datum"];
+    const stavLabe = tempsGet[0]["stav"];
+    const prutokLabe = tempsGet[0]["prutok"];
+    const teplotaLabe = tempsGet[0]["teplota"];
   });
 });
